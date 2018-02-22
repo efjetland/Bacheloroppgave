5847bad1844e74fca3fd7da31c703c8f8e9ef210
efjetland <efjetland@csumb.edu>
2018-02-22 15:32:26 +0100

    Rewrote bluetooth code for improved functionality

--- test2.py
+++ test2.py
@@ -2,6 +2,58 @@ from gattlib import DiscoveryService, GATTRequester
 import pexpect
 import time
 
+class Child:
+    __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.spawn = pexpect.spawn("gatttool -t random -I -b {0}".format(address))
+        self.isRunning = False
+
+    def getName(self):
+        return self.name
+
+    def getAddress(self):
+        return self.address
+
+    def setName(self, name):
+        self.name = name
+
+    def connect(self):
+        self.spawn.sendline("connect")
+        self.spawn.expect("Connection successful", timeout=10)
+        return True
+
+    def disconnect(self):
+        self.spawn.sendline("disconnect")
+        self.spawn.sendline("quit")
+        return True
+    def start(self):
+        self.spawn.sendline("char-write-req 0x0011 0100")
+        self.spawn.expect("Characteristic value was written successfully")
+        self.isRunning = True
+        return True
+
+    def stop(self):
+        self.spawn.sendline("char-write-req 0x0011 0000")
+        self.spawn.expect("Characteristic value was written successfully")
+        self.isRunning = False
+        return True
+
+    def fetch_data(self):
+        if self.isRunning:
+            self.spawn.expect("value:")
+            self.spawn.expect("\n")
+            return spawn.before()
+        else:
+            return -1
+
+    def battery_level(self):
+        self.spawn.sendline("char-read-hnd 0x0041")
+        self.spawn.expect("Characteristic value/descriptor:")
+        data = self.spawn.before()
+        return str(int(data, 16)) + "%"
+
+
 service = DiscoveryService("hci0")
 devices = service.discover(2)
 device_list = []
@@ -13,7 +65,14 @@ for address, name in devices.items():
         device_list.append(address)
 
 children = []
-for address in device_list:
+for ind, address in enumerate(device_list):
+    child = Child(str(ind), address)
+    child.connect()
+    print("Battery level: " + child.battery_level())
+    child.start()
+    children.append(child)
+
+    '''
     print("Running gatttool")
     child = pexpect.spawn("gatttool -t random -I -b {0}".format(address))
     print("Connecting to: {}".format(address))
@@ -27,11 +86,31 @@ for address in device_list:
     child.sendline("char-write-req 0x0011 0100")
     child.expect("Characteristic value was written successfully")
     children.append(child)
+    '''
 
 log = open("log.txt", "w+")
 log.write("\n\n---- Starting logging at: " + time.ctime(time.time()) + " ---- \n\n")
 while True:
     for ind, child in enumerate(children):
+        data = child.fetch_data()
+        if data != -1:
+            print(data)
+            log.write(data)
+            data = data.strip().split(" ")
+            print("BPM: {}".format(int(data[1], 16)))
+            log.write("BPM: {}".format(int(data[1], 16)))
+            if (int(data[0],16)&(1<<4)) != 0:
+                iterator = iter(data[2:])
+                for i, j in enumerate(iterator):
+                    j = j + next(iterator)
+                    k = int(j, 16)
+                    print("RR-interval-{}: {}\n".format(i, k))
+                    log.write("RR-interval-{}: {}\n".format(i, k))
+        else:
+            print("Error fetching data")
+
+
+        '''
         child.expect("\n")
         print("\n\nSensor {}: \n".format(ind))
         log.write("\n\nSensor {}: \n".format(ind))
@@ -48,3 +127,4 @@ while True:
                     k = int(j, 16)
                     print("RR-Interval-{}: {}\n".format(i, k))
                     log.write("RR-Interval-{}: {}\n".format(i, k))
+        '''
